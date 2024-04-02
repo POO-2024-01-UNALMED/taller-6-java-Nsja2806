f84ec936f31110a9a2dac6bcc05f96353aa3af73
Nsja2806 <nizambranom@unal.edu.co>
2024-04-02 08:54:20 -0500

    3

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -3,39 +3,40 @@ package vehiculos;
 import java.util.ArrayList;
 
 public class Pais {
-    private String nombre;
-    private int cantidadVentas = 0;
-    private static ArrayList<Pais> ventasPaises = new ArrayList<Pais>();
+
+        private String nombre;
+        private int cantidadVentas = 0;
+        private static ArrayList<Pais> ventasPaises = new ArrayList<Pais>();
 
     // Constructor
-    public Pais(String nombre) {
-        this.nombre = nombre;
-    }
+        public Pais(String nombre) {
+            this.nombre = nombre;
+        }
 
     // Getters & Setters
-    public String getNombre() {
-        return nombre;
-    }
+        public String getNombre() {
+            return nombre;
+        }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
 
-    public int getVentasRealizadas() {
-        return cantidadVentas;
-    }
+        public int getVentasRealizadas() {
+            return cantidadVentas;
+        }
 
-    public void ventasRealizadas() {
-        this.cantidadVentas++;
-    }
+        public void ventasRealizadas() {
+            this.cantidadVentas++;
+        }
 
-    public static Pais paisMasVendedor() {
-		Pais masVendedor = ventasPaises.get(0);
-		for (Pais pais:ventasPaises) {
-			if (pais.getVentasRealizadas() > masVendedor.getVentasRealizadas()) {
-				masVendedor = pais;
-			}
-		}
-		return masVendedor;
-	}
+        public static Pais paisMasVendedor() {
+		    Pais masVendedor = ventasPaises.get(0);
+		    for (Pais pais:ventasPaises) {
+			    if (pais.getVentasRealizadas() > masVendedor.getVentasRealizadas()) {
+				    masVendedor = pais;
+			    }
+		    }
+		    return masVendedor;
+	    }
 }
